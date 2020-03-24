f452794bf62e50e73033db26df2d78501e2f4d4c
rackaracka <matte.hj1234@gmail.com>
2020-03-24 16:55:37 +0100

    Kommentarer

--- include/core/Entity.h
+++ include/core/Entity.h
@@ -12,9 +12,14 @@ typedef struct Entity{
     SDL_bool isMovable;
     Drawable drawable;
 }Entity;
+//Returns an entity struct that contains the nessecary arguments for a basic funktional entity
 Entity EntityCreate(int x, int, int moveSpeed, int rotSpeed, EntityPresets preset, SDL_bool isCollider, SDL_bool isMovable);
+//Draw the entity relative to the camera
 void EntityDraw(Camera *camera, Entity entity);
+//Updates the targeted entity and ignores collision with any other entity
 void EntityUpdate(Entity *entity, Clock *clk);
+//Updates the targeted entity using the EntityOnCollision method
 void EntityUpdateWithCollision(Entity entities[], int nrEnts, Entity *user, int nrSelfIndex, Clock *clk);
+//Returns a SDL_bool based on if the user entity is colliding with anything else in the entities array, nrEnts are length of array, nrSelfIndex is the index in the array in which the entity is located at (if it doesnt exist in that context simply put a higher number than the nrEnts)
 SDL_bool EntityOnCollision(Entity entities[], int nrEnts, Entity user, int nrSelfIndex, Clock *clk);
 #endif
