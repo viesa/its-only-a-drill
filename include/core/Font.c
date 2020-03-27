735f4cd42c5a0bdde6159f7252fc447652f33470
Pierre <pierrelfevre@gmail.com>
2020-03-27 15:08:39 +0100

    hella vaporwave 3d text, simpler aligning of fonts

--- include/core/Font.c
+++ include/core/Font.c
@@ -22,17 +22,36 @@ Font *FontCreate(Graphics *gfx)
     return font;
 }
 
-void FontDraw(Font *font, FontSheet fontEnum, char text[], int x, int y, SDL_Color color)
+void FontDraw(Font *font, FontSheet fontEnum, char text[], int x, int y, FontAlign align, SDL_Color color)
 {
+    int alignOffsetX = 0;
+
+    //Calculate destination from alignment
+    SDL_Rect drawSize = FontGetSize(font, fontEnum, text);
+
+    switch (align)
+    {
+    case FAL_L:
+        break;
+    case FAL_C:
+        alignOffsetX = drawSize.w / 2;
+        break;
+    case FAL_R:
+        alignOffsetX = drawSize.w;
+        break;
+    }
+
     SDL_Surface *surface = TTF_RenderText_Solid(font->fonts[fontEnum], text, color);
     SDL_Texture *texture = SDL_CreateTextureFromSurface(GraphicsGetRenderer(font->gfx), surface);
 
     int texW = 0;
     int texH = 0;
     SDL_QueryTexture(texture, NULL, NULL, &texW, &texH);
-    SDL_Rect dstrect = {x, y, texW, texH};
+    SDL_Rect dstrect = {x - alignOffsetX, y, texW, texH};
 
     SDL_RenderCopy(GraphicsGetRenderer(font->gfx), texture, NULL, &dstrect);
+
+    //SDL_RenderCopyEx(GraphicsGetRenderer(font->gfx), texture, NULL, &dstrect, 90, &(SDL_Point){0, 0}, SDL_FLIP_NONE);
     SDL_DestroyTexture(texture);
     SDL_FreeSurface(surface);
 }
@@ -52,10 +71,41 @@ SDL_Rect FontGetSize(Font *font, FontSheet fontEnum, char text[])
     SDL_FreeSurface(surface);
 }
 
-void FontDraw3D(Font *font, FontSheet fontEnum, char text[], int x, int y, int offset, SDL_Color color1, SDL_Color color2)
+void FontDraw3D(Font *font, FontSheet fontEnum, char text[], int x, int y, FontAlign align, int offset, Font3dDirection dir, int layers, SDL_Color color[])
 {
-    FontDraw(font, TTF_Robot_Crush, text, x - offset, y - offset, color1);
-    FontDraw(font, TTF_Robot_Crush, text, x, y, color2);
+    for (size_t i = 0; i < layers; i++)
+    {
+        switch (dir)
+        {
+        case F3D_TL:
+            FontDraw(font, TTF_Robot_Crush, text, x - offset * i, y - offset * i, align, color[i]);
+            break;
+        case F3D_TC:
+            FontDraw(font, TTF_Robot_Crush, text, x, y - offset * i, align, color[i]);
+            break;
+        case F3D_TR:
+            FontDraw(font, TTF_Robot_Crush, text, x + offset * i, y - offset * i, align, color[i]);
+            break;
+        case F3D_CL:
+            FontDraw(font, TTF_Robot_Crush, text, x - offset * i, y, align, color[i]);
+            break;
+        case F3D_CC:
+            FontDraw(font, TTF_Robot_Crush, text, x, y, align, color[i]);
+            break;
+        case F3D_CR:
+            FontDraw(font, TTF_Robot_Crush, text, x + offset * i, y, align, color[i]);
+            break;
+        case F3D_BL:
+            FontDraw(font, TTF_Robot_Crush, text, x - offset * i, y + offset * i, align, color[i]);
+            break;
+        case F3D_BC:
+            FontDraw(font, TTF_Robot_Crush, text, x, y + offset * i, align, color[i]);
+            break;
+        case F3D_BR:
+            FontDraw(font, TTF_Robot_Crush, text, x + offset * i, y + offset * i, align, color[i]);
+            break;
+        }
+    }
 }
 
 void FontDestroy(Font *font)
--- include/core/Font.h
+++ include/core/Font.h
@@ -12,6 +12,31 @@ typedef enum FontSheet
     TTF_Count
 } FontSheet;
 
+// Font align enum.
+// FAL_(X) [Left, Center, Right]
+typedef enum FontAlign
+{
+    FAL_L,
+    FAL_C,
+    FAL_R
+} FontAlign;
+
+// Font 3d drawing direction.
+// F3D_[1][2]: 1[(T)Top, (C)Center, (B)Bottom]
+// F3D_[1][2]: 2[(L)Left, (C)Center, (R)Right]
+typedef enum Font3dDirection
+{
+    F3D_TL,
+    F3D_TC,
+    F3D_TR,
+    F3D_CL,
+    F3D_CC,
+    F3D_CR,
+    F3D_BL,
+    F3D_BC,
+    F3D_BR
+} Font3dDirection;
+
 typedef struct Font
 {
     Graphics *gfx;
@@ -19,9 +44,9 @@ typedef struct Font
 } Font;
 
 Font *FontCreate(Graphics *gfx);
-void FontDraw(Font *font, FontSheet fontEnum, char text[], int x, int y, SDL_Color color);
+void FontDraw(Font *font, FontSheet fontEnum, char text[], int x, int y, FontAlign align, SDL_Color color);
 SDL_Rect FontGetSize(Font *font, FontSheet fontEnum, char text[]);
-void FontDraw3D(Font *font, FontSheet fontEnum, char text[], int x, int y, int offset, SDL_Color color1, SDL_Color color2);
+void FontDraw3D(Font *font, FontSheet fontEnum, char text[], int x, int y, FontAlign align, int offset, Font3dDirection dir, int layers, SDL_Color color[]);
 void FontDestroy(Font *font);
 
 #endif
