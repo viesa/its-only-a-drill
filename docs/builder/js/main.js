de7faf8dec0af268f5fb0f588fad776daa7a8c59
Pierre <pierrelfevre@gmail.com>
2020-03-31 14:42:53 +0200

    level builder update, now able to change values on the fly.

--- docs/builder/js/main.js
+++ docs/builder/js/main.js
@@ -33,13 +33,50 @@ $("#canvas").on("click", function () {
     }
 });
 
+$(document).on("change", '.lrval', function () {
+    switch ($(this).attr("lrType")) {
+        case "lrvx":
+            imageList[$(this).parent().parent().attr("chnr")].x = $(this).val();
+            break;
+
+        case "lrvy":
+            imageList[$(this).parent().parent().attr("chnr")].y = $(this).val();
+            break;
+
+        case "lrvw":
+            imageList[$(this).parent().parent().attr("chnr")].w = $(this).val();
+            break;
+
+        case "lrvh":
+            imageList[$(this).parent().parent().attr("chnr")].h = $(this).val();
+            break;
+    }
+    update();
+});
+
+function lrdel(element) {
+    imageList.splice(element.parent().parent().attr("chnr"), 1);
+    update();
+};
+
 function update() {
     $("#layers-title").text("layers (" + imageList.length + ")");
 
     var container = $("#layers-container");
     container.empty();
     for (i = 0; i < imageList.length; i++) {
-        container.append("<p class='layer-child'> Layer [" + i + "], Type: " + imageList[i].type + " X<input type='number' class='lrval lrvx' value='" + imageList[i].x + "'> Y<input type='number' class='lrval lrvy' value='" + imageList[i].y + "'> Width<input type='number' class='lrval lrvw' value='" + imageList[i].w + "'> Height<input type='number' class='lrval lrvh' value='" + imageList[i].h + "'></p>");
+        var str1 = "<div class='layer-child' chnr='" + i + "'>";
+
+        var str2 = "<div class='layer-child-box'><label>Layer [" + i + "]</label></div>";
+        var str3 = "<div class='layer-child-box'><label>Type: " + imageList[i].type + "</label></div>";
+        var str4 = "<div class='layer-child-box'><label>X</label><input type='number' class='lrval' lrType='lrvx' value='" + imageList[i].x + "'></div>";
+        var str5 = "<div class='layer-child-box'><label>Y</label><input type='number' class='lrval' lrType='lrvy' value='" + imageList[i].y + "'></div>";
+        var str6 = "<div class='layer-child-box'><label>W</label><input type='number' class='lrval' lrType='lrvw' value='" + imageList[i].w + "'></div>";
+        var str7 = "<div class='layer-child-box'><label>H</label><input type='number' class='lrval' lrType='lrvh' value='" + imageList[i].h + "'></div>";
+        var str8 = "<div class='layer-child-box'><button class='lrdel' onclick='lrdel($(this))'>Delete</button></div>";
+        var str9 = "</div>";
+
+        container.append(str1 + str2 + str3 + str4 + str5 + str6 + str7 + str8 + str9);
     }
 
     clearCanvas();
