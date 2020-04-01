ecfa41a06d88840d161d64f2785e131422bbbd98
jacdav <lundin00@live.se>
2020-04-01 11:50:18 +0200

    Score system implemented

---
+++ include/core/Score.c
@@ -0,0 +1,30 @@
+#include "Score.h"
+#include "stdio.h"
+
+struct Score
+{
+    int score;
+    double multi;
+}Player[4];
+
+void ScoreCreate(int id) 
+{
+    Player[id].score = 0;
+    Player[id].multi = 1.0;
+}
+
+int ScoreInfo(int id)
+{
+    return Player[id].score;
+}
+
+void ScoreIncrement(int plus,int id)
+{
+    Player[id].score += (plus * Player[id].multi);
+}
+
+void ScoreMultiChange(double new,int id) 
+{
+    Player[id].multi = new;
+}
+
