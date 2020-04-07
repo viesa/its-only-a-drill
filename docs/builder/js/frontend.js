b4b08d348518c3c59183fd3a4d9bd53b4ecc5eb1
Pierre <pierrelfevre@gmail.com>
2020-04-07 13:48:18 +0200

    Reorganize layer display in level builder

--- docs/builder/js/frontend.js
+++ docs/builder/js/frontend.js
@@ -4,20 +4,34 @@ function frontendUpdate() {
     for (i = 0; i < db.list.length; i++) {
         var str1 = "<div class='layer-child' chnr='" + i + "'>";
 
-        var str2 = "<div class='layer-child-box'><label>Layer [" + i + "]</label></div>";
-        var str3 = "<div class='layer-child-box'><label>Type: " + db.list[i].type + "</label></div>";
-        var str4 = "<div class='layer-child-box'><label>X</label><input type='number' class='lrval' lrType='lrvx' value='" + db.list[i].x + "'></div>";
-        var str5 = "<div class='layer-child-box'><label>Y</label><input type='number' class='lrval' lrType='lrvy' value='" + db.list[i].y + "'></div>";
-        var str6 = "<div class='layer-child-box'><label>Width</label><input type='number' class='lrval' lrType='lrvw' value='" + db.list[i].w + "'></div>";
-        var str7 = "<div class='layer-child-box'><label>Height</label><input type='number' class='lrval' lrType='lrvh' value='" + db.list[i].h + "'></div>";
-        var str8 = "<div class='layer-child-box'><label>Mass</label><input type='number' class='lrval' lrType='lrvm' value='" + db.list[i].m + "'></div>";
-        var str9 = "<div class='layer-child-box'><label>Collider</label><input type='number' class='lrval' lrType='lrvc' value='" + db.list[i].c + "'></div>";
-        var str10 = "<div class='layer-child-box'><label>Rotation</label><input type='number' class='lrval' lrType='lrvr' value='" + db.list[i].r + "'></div>";
-        var str11 = "<div class='layer-child-box'><button class='lrbutton lrdel' onclick='lrdel($(this))'>Delete</button><button class='lrbutton lrhigh' onclick='highLight($(this))'>Highlight</button></div>";
-        var str12 = "<div class='layer-child-box'><button class='lrbutton lrup' onclick='lrup($(this))'>Up</button><button class='lrbutton lrdown' onclick='lrdown($(this))'>Down</button></div>";
-        var str13 = "</div>";
+        var str2 = "<div class='layer-child-first'><div class='layer-child-info'><label>Z-Index: " + i + "</label>";
+        var str3 = "<label>Type: " + db.list[i].type + "</label></div><div class='layer-child-options'>";
+        var str4 = "<button class='lrbutton lrexpand'>Options</button>";
+        var str5 = "<button class='lrbutton lrdel' onclick='lrdel($(this))'>Delete</button>";
+        var str6 = "<button class='lrbutton lrhigh' onclick='highLight($(this))'>Highlight</button>";
 
-        container.append(str1 + str2 + str3 + str4 + str5 + str6 + str7 + str8 + str9 + str10 + str11 + str12 + str13);
+        if (i > 0) {
+            var str7 = "<button class='lrbutton lrup lrarrow' onclick='lrup($(this))'><i class='fas fa-arrow-up'></i></button>";
+        } else {
+            var str7 = "<button class='lrbutton lrup lrarrow lrdisabled' disabled onclick='lrup($(this))'><i class='fas fa-arrow-up'></i></button>";
+        }
+
+        if (i < db.list.length - 1) {
+            var str8 = "<button class='lrbutton lrdown lrarrow' onclick='lrdown($(this))'><i class='fas fa-arrow-down'></i></button>";
+        } else {
+            var str8 = "<button class='lrbutton lrdown lrarrow lrdisabled' disabled onclick='lrdown($(this))'><i class='fas fa-arrow-down'></i></button>";
+        }
+        var str9 = "</div></div><div class='layer-child-expanded' style='display:none'>";
+        var str10 = "<div class='layer-child-box'><label>X</label><input type='number' class='lrval' lrType='lrvx' value='" + db.list[i].x + "'></div>";
+        var str11 = "<div class='layer-child-box'><label>Y</label><input type='number' class='lrval' lrType='lrvy' value='" + db.list[i].y + "'></div>";
+        var str12 = "<div class='layer-child-box'><label>Width</label><input type='number' class='lrval' lrType='lrvw' value='" + db.list[i].w + "'></div>";
+        var str13 = "<div class='layer-child-box'><label>Height</label><input type='number' class='lrval' lrType='lrvh' value='" + db.list[i].h + "'></div>";
+        var str14 = "<div class='layer-child-box'><label>Mass</label><input type='number' class='lrval' lrType='lrvm' value='" + db.list[i].m + "'></div>";
+        var str15 = "<div class='layer-child-box'><label>Collider</label><input type='number' class='lrval' lrType='lrvc' value='" + db.list[i].c + "'></div>";
+        var str16 = "<div class='layer-child-box'><label>Rotation</label><input type='number' class='lrval' lrType='lrvr' value='" + db.list[i].r + "'></div>";
+        var str17 = "</div></div>";
+
+        container.append(str1 + str2 + str3 + str4 + str5 + str6 + str7 + str8 + str9 + str10 + str11 + str12 + str13 + str14 + str15 + str16 + str17);
     }
 }
 
@@ -55,12 +69,12 @@ $(document).on("change", '.lrval', function () {
 });
 
 function lrdel(element) {
-    db.list.splice(element.parent().parent().attr("chnr"), 1);
+    db.list.splice(element.parent().parent().parent().attr("chnr"), 1);
     update();
 }
 
 function lrup(element) {
-    var index = parseInt(element.parent().parent().attr("chnr"));
+    var index = parseInt(element.parent().parent().parent().attr("chnr"));
     var a = db.list[index - 1];
     db.list[index - 1] = db.list[index];
     db.list[index] = a;
@@ -68,7 +82,7 @@ function lrup(element) {
 }
 
 function lrdown(element) {
-    var index = parseInt(element.parent().parent().attr("chnr"));
+    var index = parseInt(element.parent().parent().parent().attr("chnr"));
     var a = db.list[index];
     db.list[index] = db.list[index + 1];
     db.list[index + 1] = a;
@@ -105,6 +119,7 @@ $("#canvas").on("click", function () {
         db.list.push(objectGenerator(selectedLayer.attr("type"), document.getElementById(selectedLayer.attr("id")), parseInt(mouseX), parseInt(mouseY), cvW * 2, cvH * 2, parseInt(selectedLayer.attr("mass")), parseInt(selectedLayer.attr("collider")), parseInt(0), cvX, cvY, cvW, cvH));
 
         update();
+        $('#layers-container').scrollTop($('#layers-container')[0].scrollHeight);
     }
     else {
         alert("You need to select a type first.");
@@ -113,6 +128,10 @@ $("#canvas").on("click", function () {
 
 function highLight(object) {
     update();
-    var hoverObj = db.list[object.parent().parent().attr("chnr")];
+    var hoverObj = db.list[object.parent().parent().parent().attr("chnr")];
     drawHighlightRectangle(hoverObj.x, hoverObj.y, hoverObj.w, hoverObj.h, hoverObj.r);
 };
+
+$(document).on("click", '.lrexpand', function () {
+    $(this).parent().parent().parent().find(".layer-child-expanded").slideToggle("fast");
+});
