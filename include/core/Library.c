a6616b898e35dc2d8cf77498b117bc34e04a20fb
viesa <own.emka@gmail.com>
2020-04-08 14:26:28 +0200

    Add new function to Library (toDegrees)

--- include/core/Library.c
+++ include/core/Library.c
@@ -31,3 +31,8 @@ Vec2 RectMid(SDL_Rect rect)
 {
     return Vec2Create((float)rect.x + (float)rect.w / 2.0f, (float)rect.y + (float)rect.h / 2.0f);
 }
+
+double toDegrees(double radians)
+{
+    return (radians * 180) / PI_D;
+}
