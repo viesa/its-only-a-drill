7116403dc4460546d861d7b8e4cf1f599f817856
stiroy <stiroy.xron@gmail.com>
2020-04-20 13:54:09 +0200

    Update Library.c has dynamic functions

--- include/core/Library.c
+++ include/core/Library.c
@@ -1,32 +1,5 @@
 #include "Library.h"
 
-float min(float f1, float f2)
-{
-    if (f2 < f1)
-    {
-        return f2;
-    }
-    else
-    {
-        return f1;
-    }
-}
-float max(float f1, float f2)
-{
-    if (f2 > f1)
-    {
-        return f2;
-    }
-    else
-    {
-        return f1;
-    }
-}
-float sq(float x)
-{
-    return x * x;
-}
-
 Vec2 RectMid(SDL_Rect rect)
 {
     return Vec2Create((float)rect.x + (float)rect.w / 2.0f, (float)rect.y + (float)rect.h / 2.0f);
