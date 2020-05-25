ede9301244a76882c20d025e7a177a34bd5b2a4e
Mats-HJalmar Murman <matte.hj1234@gmail.com>
2020-05-25 10:05:04 +0200

    Removed un-needed code in weapon.h

--- include/env/Weapon.h
+++ include/env/Weapon.h
@@ -35,20 +35,13 @@ const void WeaponSingleplayerHandler(int *index, Vec2 *direction, WeaponStats *S
 void WeaponRayScan(EntityIndexP source, Vec2 *direction, WeaponStats *stats, void *pointerToFunc);
 // takes steps intil collision
 void WeaponRayScanClosest(EntityIndexP source, Vec2 *direction, WeaponStats *stats, void *pointerToFunc);
-/// gives the maximum distans before collision
+/// gives the maximum distance before collision
 ///\param index: index == zero to not test with anything
 /// otherwise ignore index entity
 float WeaponMaxDistanceBeforeColision(Vec2 point, EntityIndexP index, float maxDistance);
-/// returns 1 if a hit and changes data, returns 0 if failed
+/// returns 1 if hit and changes data, returns 0 if failed
 SDL_bool WeaponTestLineWithEntities(Vec2 start, Vec2 end, EntityIndexP ignoreEntity, int *damage);
 // casts a ray and changes data on closest hit
 void RayScanClosest(EntityIndexP source, Vec2 *direction, WeaponStats *stat, void *pointerToFunc);
-// skapar en projektil *unsused/not updated
-// void bullet(int index, Vec2 Destination, SDL_Point point, Item *item, Vec2 Direction);
-
-// draws a color with a preset color (red currently)
-void DrawLineOnCanvas(SDL_Renderer *renderer, int x1, int y1, int x2, int y2);
-// reserved for project based weapons
-void DectectIntersectionKeep();
 
 #endif
